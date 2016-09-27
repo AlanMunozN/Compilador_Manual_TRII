e44feac38931a96e17960035ada6ed48e1712bcc
ckmu32 <kalebmercado@icloud.com>
2016-09-27 15:32:07 -0500

    Guardado y apertura de archivos correcta. Manejo de errores y despliegue de la venta del léxico funcional

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,19 +1,41 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TreeView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.fxmisc.richtext.CodeArea;
 import org.fxmisc.richtext.LineNumberFactory;
 import org.fxmisc.richtext.model.StyleSpans;
 import org.fxmisc.richtext.model.StyleSpansBuilder;
 
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Controller {
+public class Controller implements Initializable{
     @FXML
     CodeArea codeArea;
+    @FXML
+    TextArea txtMensajes;
+    @FXML
+    ListView lsErrores;
 
     private static final String[] KEYWORDS = new String[] {
             "abstract", "assert", "boolean", "break", "byte",
@@ -46,16 +68,97 @@ public class Controller {
                     + "|(?<COMMENT>" + COMMENT_PATTERN + ")"
     );
 
-
-    public void cargarArchivo(){
+    public void initialize(URL location, ResourceBundle resources){
         colorPalabras_Reservadas();
         contadorLineas();
+        inicializaErrores_Prueba();
+    }
+
+    File archivo;
+
+    public void cargarArchivo() throws IOException{
+        FileChooser fileChooser = new FileChooser();            //Nos deja seleccionar un archivo a cargar
+        fileChooser.setTitle("Seleccione un archivo para cargar");
+        archivo = fileChooser.showOpenDialog(new Stage());//Obtenemos ruta
+
+        if(archivo != null) {
+            FileInputStream stream = new FileInputStream(archivo);
+            //Declaramos el encoding de entrada
+            BufferedReader entrada = new BufferedReader(new InputStreamReader(stream, Charset.forName("UTF-8")));
+
+            String linea = null;
+            while ((linea = entrada.readLine()) != null) {//Lee archivo y lo pone en el textArea
+                codeArea.appendText(linea + "\n");
+            }
+            entrada.close();
+
+            //txtArea.appendText(rutaArchivo);
+            txtMensajes.setText("");
+            txtMensajes.setText("Archivo cargado con éxito");
+        }
+        else {
+            txtMensajes.setText("");
+            txtMensajes.setText("Error de carga en archivo");
+        }
+
+        /*
         codeArea.appendText("Hola\n");
         codeArea.appendText("Mundo\n");
         codeArea.appendText("Gitano priemra segunda tercera cuarta");
-        codeArea.moveTo(1,0);//Primer digito comenzando de 0 indica linea, segundo indica columna
-        codeArea.selectLine();//Resalta toda la linea
-        System.out.println(codeArea.getText(2));//Obtenemos el texto de la linea 3 con el getText. SOLO PRUEBA
+        */
+    }
+
+    String contenidoArchivo = new String();
+    public void guardarArchivo () throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Guardar archivo");
+
+        //Extensión
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        FileChooser.ExtensionFilter extFilter2 = new FileChooser.ExtensionFilter("RTF files (*.rtf)", "*.rtf");
+        fileChooser.getExtensionFilters().addAll(extFilter,extFilter2);
+
+        //Muestra dialogo
+        archivo = fileChooser.showSaveDialog(new Stage());
+
+        contenidoArchivo = (String) codeArea.getText();//Obtenemos contenido del textArea y la pasamosa string
+
+        if(archivo != null) {
+            SaveFile(contenidoArchivo,archivo);
+            txtMensajes.setText("");
+            txtMensajes.setText("Archivo guardado con éxito");
+        }
+        else {
+            txtMensajes.setText("");
+            txtMensajes.setText("Error en guardado de archivo");
+        }
+
+    }
+
+    private void SaveFile(String content, File file){
+        try {
+            FileWriter fileWriter = null;
+
+            fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public void guardarRapido () { // Guarda el archivo si previamente fue guardado como. No pregunta
+        if(contenidoArchivo.equals("")) {
+            txtMensajes.setText("");
+            txtMensajes.setText("Primero ocupa guardar como");
+        }
+        else {
+            String contenido = (String) codeArea.getText();
+            SaveFile(contenido, archivo);
+            txtMensajes.setText("");
+            txtMensajes.setText("Archivo guardado rápido con éxito");
+        }
     }
 
     public void contadorLineas(){
@@ -91,5 +194,28 @@ public class Controller {
         }
         spansBuilder.add(Collections.emptyList(), text.length() - lastKwEnd);
         return spansBuilder.create();
+    }
+
+    public void inicializaErrores_Prueba(){
+        ObservableList<Integer> items = FXCollections.observableArrayList(1,2,3,4);
+        lsErrores.setItems(items);
+    }
+
+    public void manejoErrores(){
+        int valor = lsErrores.getSelectionModel().getSelectedIndex();
+        //Marcado de linea
+        codeArea.moveTo(valor,0);//Primer digito comenzando de 0 indica linea, segundo indica columna
+        codeArea.selectLine();//Resalta toda la linea
+        //System.out.println(codeArea.getText(2));//Obtenemos el texto de la linea 3 con el getText. SOLO PRUEBA
+    }
+
+    public void abrirLexico() throws IOException{
+        Stage primaryStage=new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("Lexico.fxml"));
+        root.getStylesheets().add(Controller.class.getResource("java-keywords.css").toExternalForm());//Importamos el css a usar
+        primaryStage.setTitle("Compilador Manual - Traductores II");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+
     }
 }
